df48733bd5fddb56ad2c9d22d10d30cbdf095036
omartinez <omartinez@ifae.es>
2022-03-25 14:41:12 +0100

    Added class101 examples

---
+++ Class101/src/main.cpp
@@ -0,0 +1,113 @@
+#include <Arduino.h>
+
+//Libraries for OLED Display
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+//Libraries for LoRa
+#include <SPI.h>
+#include <LoRa.h>
+
+#include "RTPPhotoDiodeTrigger.h"
+
+#define LDR_PIN 13
+#define LED_PIN 21
+#define PWM_CHANNEL 0
+#define PWM_FREQ 5000
+#define PWM_RES 8
+
+//OLED pins
+#define OLED_SDA 4
+#define OLED_SCL 15 
+#define OLED_RST 16
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+
+//define the pins used by the LoRa transceiver module
+#define SCK 5
+#define MISO 19
+#define MOSI 27
+#define SS 18
+#define RST 14
+#define DIO0 26
+
+//433E6 for Asia
+//866E6 for Europe
+//915E6 for North America
+#define BAND 866E6
+
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RST);
+RTPPhotoDiodeTrigger ldrTrigger(0, LDR_PIN);
+
+const int thresholdValue = 1000;
+
+void printToScreen(String firstLine, String secondLine, String thirdLine, String fourthLine){
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(0,0);
+  display.println(firstLine);
+  display.println(secondLine);
+  display.println(thirdLine);
+  display.println(fourthLine);
+  display.display();
+}
+
+void actOnPhotoDiodeTrigger(int ID, String callbackString){
+  printToScreen("LDR", "ID:"+String(ID), callbackString,"");
+  LoRa.beginPacket();
+  LoRa.print(callbackString);
+  LoRa.endPacket();
+}
+
+void setup() {
+  Serial.begin(115200);
+  //pinMode(LED_PIN, OUTPUT);
+  ledcSetup(PWM_CHANNEL, PWM_FREQ, PWM_RES);
+  ledcAttachPin(LED_PIN, PWM_CHANNEL);
+
+    //initialize OLED
+  pinMode(OLED_RST, OUTPUT);
+  digitalWrite(OLED_RST, LOW);
+  delay(20);
+  digitalWrite(OLED_RST, HIGH);
+  Wire.begin(OLED_SDA, OLED_SCL);
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3c, false, false)) { // Address 0x3C for 128x32
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;); // Don't proceed, loop forever
+  }
+  printToScreen("LORA SENDER", "TEST", "", "");
+
+    //SPI LoRa pins
+  SPI.begin(SCK, MISO, MOSI, SS);
+  //setup LoRa transceiver module
+  LoRa.setPins(SS, RST, DIO0);
+  
+  if (!LoRa.begin(BAND)) {
+    Serial.println("Starting LoRa failed!");
+    while (1);
+  }
+  ldrTrigger.setThreshold(thresholdValue);
+}
+
+void loop() {
+  /*int analogValue = analogRead(LDR_PIN);
+  Serial.printf("LDR value: %d\n", analogValue);
+  int pwmValue = map(analogValue, 0, 4095, 0, 255);*/
+  //ledcWrite(PWM_CHANNEL, pwmValue);
+  //printToScreen("LDR value: ", String(analogValue), "", "");
+
+  ldrTrigger.readnShoot(actOnPhotoDiodeTrigger);
+  /*
+  if(analogValue < thresholdValue){
+    digitalWrite(LED_PIN, HIGH);
+    Serial.println("LIGHT UP!");
+  }
+  else{
+    digitalWrite(LED_PIN, LOW);
+    Serial.println("LIGHT DOWN!");
+  }
+  */
+ //delay(100);
+}
---
+++ Class101RX/src/main.cpp
@@ -0,0 +1,102 @@
+#include <Arduino.h>
+
+//Libraries for LoRa
+#include <SPI.h>
+#include <LoRa.h>
+
+//Libraries for OLED Display
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+//define the pins used by the LoRa transceiver module
+#define SCK 5
+#define MISO 19
+#define MOSI 27
+#define SS 18
+#define RST 14
+#define DIO0 26
+
+//433E6 for Asia
+//866E6 for Europe
+//915E6 for North America
+#define BAND 866E6
+
+//OLED pins
+#define OLED_SDA 4
+#define OLED_SCL 15 
+#define OLED_RST 16
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+
+#define LED_PIN 21
+
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RST);
+
+String LoRaData;
+
+void printToScreen(String firstLine, String secondLine, String thirdLine, String fourthLine){
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(0,0);
+  display.println(firstLine);
+  display.println(secondLine);
+  display.println(thirdLine);
+  display.println(fourthLine); 
+  display.display();
+}
+
+void lightLed(String text){
+  digitalWrite(LED_PIN, text=="OFF");
+  printToScreen(text, "", "", "");
+}
+
+void setup() { 
+  //initialize Serial Monitor
+  Serial.begin(115200);
+
+  //initialize OLED
+  pinMode(OLED_RST, OUTPUT);
+  digitalWrite(OLED_RST, LOW);
+  delay(20);
+  digitalWrite(OLED_RST, HIGH);
+  Wire.begin(OLED_SDA, OLED_SCL);
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3c, false, false)) { // Address 0x3C for 128x32
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;); // Don't proceed, loop forever
+  }
+
+  printToScreen("LORA RECEIVER", "TEST", "", "");
+
+  Serial.println("LoRa Receiver Test");
+  
+  //SPI LoRa pins
+  SPI.begin(SCK, MISO, MOSI, SS);
+  //setup LoRa transceiver module
+  LoRa.setPins(SS, RST, DIO0);
+
+  if (!LoRa.begin(BAND)) {
+    Serial.println("Starting LoRa failed!");
+    while (1);
+  }
+
+  pinMode(LED_PIN, OUTPUT);
+}
+
+void loop() {
+  int packetSize = LoRa.parsePacket();
+  if (packetSize) {
+    int value;
+    Serial.print("Received packet ");
+    while (LoRa.available()) {
+      LoRaData = LoRa.readString();
+      Serial.print(LoRaData);
+      lightLed(LoRaData);
+      Serial.print(value);
+    }
+    int rssi = LoRa.packetRssi();
+    Serial.print("  with RSSI ");    
+    Serial.println(rssi);
+  }
+}
