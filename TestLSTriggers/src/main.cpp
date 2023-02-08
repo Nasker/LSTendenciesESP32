8215561818ce2a50c3b4ddb7740d003624c747a5
Nasker <xamps23@gmail.com>
2023-02-08 14:20:59 +0100

    Updated LSTRigger

--- TestLSTriggers/src/main.cpp
+++ TestLSTriggers/src/main.cpp
@@ -1,10 +1,14 @@
 #include <Arduino.h>
 #include "LSDigitalTrigger.h"
+#include "LSAnalogTrigger.h"
 
 #define BTN_ID 0
 #define BTN_PIN 0
+#define LDR_ID 1
+#define LDR_PIN 35
 
-LSDigitalTrigger trigger(BTN_ID, BTN_PIN);
+LSDigitalTrigger digitalTrigger(BTN_ID, BTN_PIN);
+LSAnalogTrigger analogTrigger(LDR_ID, LDR_PIN);
 
 void actOnTrigger(int ID, String callbackString){
   Serial.printf("->ID: %d went %s\n", ID, callbackString.c_str());
@@ -12,8 +16,10 @@ void actOnTrigger(int ID, String callbackString){
 
 void setup() {
   Serial.begin(115200);
+  analogTrigger.setThreshold(1000);
 }
 
 void loop() {
-  trigger.readnShoot(actOnTrigger);
+  digitalTrigger.readnShoot(actOnTrigger);
+  analogTrigger.readnShoot(actOnTrigger);
 }
